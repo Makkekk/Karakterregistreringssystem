7a462c05f9a95481a4f57ac2b4c944fb0ed053a5
Makkekk <karsten-kirkegaard@hotmail.com>
2025-02-27 09:41:26 +0100

    Add files via upload

---
+++ Karakterregistreringssystem/src/Opgave8/OpgaveA.java
@@ -0,0 +1,71 @@
+package Opgave8;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class OpgaveA {
+    public static void main(String[] args) {
+
+        try { BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+            {
+
+                Connection minConnection;
+                minConnection = DriverManager
+                        .getConnection("jdbc:sqlserver://LAPTOP-F9FN58TJ\\SQLExpress;databaseName=Karakterregistreringssystem;user=sa;password=123456;");
+
+                System.out.println("Indtast studieID: ");
+                int studieID = Integer.parseInt(reader.readLine().trim());
+
+                System.out.println("Enter eksamensAfviklingsID: ");
+                int eksamensAfviklingsID = Integer.parseInt(reader.readLine().trim());
+
+                System.out.println("Indtast karakter (-3, 0, 2, 4, 7, 10, 12): ");
+                int karakter = Integer.parseInt(reader.readLine().trim());
+
+
+                System.out.println("Indtast administrativ bedømmelse (SY, IM, IA eller tryk ENTER for ingen): ");
+                String adminBedømmelse = reader.readLine().trim();
+                if (adminBedømmelse.isEmpty()) {
+                    adminBedømmelse = null;
+
+                }
+                String beståetIkkeBestået = (karakter >= 2) ? "JA" : "NEJ";
+
+                String sql = "INSERT INTO Eksamensbesvarelse (karakter, administrativBedømmelse, BeståetIkkeBestået, studieID, EksamensAfviklingsID) " +
+                        "VALUES (?,?,?,?,?)";
+
+                try (PreparedStatement stmt = minConnection.prepareStatement(sql)) {
+                    stmt.setInt(1, karakter);
+                    stmt.setString(2, adminBedømmelse);
+                    stmt.setString(3, beståetIkkeBestået);
+                    stmt.setInt(4, studieID);
+                    stmt.setInt(5, eksamensAfviklingsID);
+
+
+                    int rowInserted = stmt.executeUpdate();
+                    if (rowInserted > 0) {
+                        System.out.println("Eksamensforsøg oprettet");
+                    } else {
+                        System.out.println("Ingen rækker blev indsat.");
+                    }
+                }
+
+            }
+        } catch (SQLException e) {
+            int errorCode = e.getErrorCode();
+
+            System.out.println(" SQL Fejl: " + e.getMessage() + e.getErrorCode());
+            if (errorCode == 547)
+                System.out.println("Input fejl: Den indskrevne karakter skal være inde for skalaen");
+        } catch (IOException e) {
+            System.out.println("Input fejl: " + e.getMessage());
+        } catch (NumberFormatException e) {
+            System.out.println("Ugyldig input! Indtast venligst tal, hvor det er nødvendigt.");
+        }
+    }
+}
---
+++ Karakterregistreringssystem/src/Opgave8/OpgaveB.java
@@ -0,0 +1,53 @@
+package Opgave8;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class OpgaveB {
+    public static void main(String[] args) {
+
+        try {
+            BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+            Connection minConnection;
+            minConnection = DriverManager
+                    .getConnection("jdbc:sqlserver://LAPTOP-F9FN58TJ\\SQLExpress;databaseName=Karakterregistreringssystem;user=sa;password=123456;");
+
+            // Læser input fra brugeren
+            System.out.println("Indtast termin for eksamensafvikling (f.eks. '2025-01'): ");
+            String termin = reader.readLine().trim();
+
+            System.out.println("Indtast startdato for eksamensafvikling (f.eks. '2025-01-10'): ");
+            String startdato = reader.readLine().trim();
+
+            System.out.println("Indtast slutdato for eksamensafvikling (f.eks. '2025-01-15'): ");
+            String slutdato = reader.readLine().trim();
+
+            System.out.println("Indtast eksamensID for eksamenen, der skal afvikles: ");
+            int eksamensID = Integer.parseInt(reader.readLine().trim());
+
+            String sql = "INSERT INTO EksamensAfvikling (termin, startsDato, slutsDato, eksamensID) VALUES (?, ?, ?, ?)";
+
+            PreparedStatement stmt = minConnection.prepareStatement(sql);
+            stmt.setString(1, termin);
+            stmt.setString(2, startdato);
+            stmt.setString(3, slutdato);
+            stmt.setInt(4, eksamensID);
+
+      
+            int rowsInserted = stmt.executeUpdate();
+            if (rowsInserted > 0) {
+                System.out.println("Eksamensafvikling oprettet succesfuldt!");
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
